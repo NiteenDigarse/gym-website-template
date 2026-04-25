29475b9ab658c8d227a42d0b16b5ffd365034e95
Niteen Digarse <niteendigarse@gmail.com>
2026-04-25 14:44:49 +0530

    Initial website upload

---
+++ js/main.js
@@ -0,0 +1,230 @@
+/*  ---------------------------------------------------
+  Template Name: Gym
+  Description:  Gym Fitness HTML Template
+  Author: Colorlib
+  Author URI: https://colorlib.com
+  Version: 1.0
+  Created: Colorlib
+---------------------------------------------------------  */
+
+'use strict';
+
+(function ($) {
+    /*------------------
+        Smooth Scrolling
+    --------------------*/
+    $('a[href^="#"]:not([href="#"])').on('click', function(e) {
+        var target = $(this.getAttribute('href'));
+        if (target.length) {
+            e.preventDefault();
+            $('html, body').stop().animate({
+                scrollTop: target.offset().top - 50
+            }, 800);
+        }
+    });
+    /*------------------
+        Enroll Now Button
+    --------------------*/
+    // Enroll Now Modal
+    $(document).on('click', '.pricing-btn', function(e) {
+        e.preventDefault();
+        $('#enroll-modal').fadeIn(200);
+    });
+    $(document).on('click', '#enroll-modal-close', function() {
+        $('#enroll-modal').fadeOut(200);
+    });
+    // Optional: Close modal on outside click
+    $(document).on('mousedown', function(e) {
+        var modal = $('#enroll-modal > div');
+        if ($('#enroll-modal').is(':visible') && !modal.is(e.target) && modal.has(e.target).length === 0) {
+            $('#enroll-modal').fadeOut(200);
+        }
+    });
+
+    /*------------------
+        Preloader
+    --------------------*/
+    $(window).on('load', function () {
+        $(".loader").fadeOut();
+        $("#preloder").delay(200).fadeOut("slow");
+    });
+
+    /*------------------
+        Background Set
+    --------------------*/
+    $('.set-bg').each(function () {
+        var bg = $(this).data('setbg');
+        $(this).css('background-image', 'url(' + bg + ')');
+    });
+
+    //Canvas Menu
+    $(".canvas-open").on('click', function () {
+        $(".offcanvas-menu-wrapper").addClass("show-offcanvas-menu-wrapper");
+        $(".offcanvas-menu-overlay").addClass("active");
+    });
+
+    $(".canvas-close, .offcanvas-menu-overlay").on('click', function () {
+        $(".offcanvas-menu-wrapper").removeClass("show-offcanvas-menu-wrapper");
+        $(".offcanvas-menu-overlay").removeClass("active");
+    });
+
+    // Search model
+    $('.search-switch').on('click', function () {
+        $('.search-model').fadeIn(400);
+    });
+
+    $('.search-close-switch').on('click', function () {
+        $('.search-model').fadeOut(400, function () {
+            $('#search-input').val('');
+        });
+    });
+
+    //Masonary
+    $('.gallery').masonry({
+        itemSelector: '.gs-item',
+        columnWidth: '.grid-sizer',
+        gutter: 10
+    });
+
+    /*------------------
+		Navigation
+	--------------------*/
+    $(".mobile-menu ul").slicknav({
+        prependTo: '#mobile-menu-wrap',
+        allowParentLinks: true
+    });
+
+    /*------------------
+        Carousel Slider
+    --------------------*/
+    var hero_s = $(".hs-slider");
+    hero_s.owlCarousel({
+        loop: true,
+        margin: 0,
+        nav: true,
+        items: 1,
+        dots: false,
+        animateOut: 'fadeOut',
+        animateIn: 'fadeIn',
+        navText: ['<i class="fa fa-angle-left"></i>', '<i class="fa fa-angle-right"></i>'],
+        smartSpeed: 1200,
+        autoHeight: false,
+        autoplay: false
+    });
+
+    /*------------------
+        Team Slider
+    --------------------*/
+    $(".ts-slider").owlCarousel({
+        loop: true,
+        margin: 0,
+        items: 3,
+        dots: true,
+        dotsEach: 2,
+        smartSpeed: 1200,
+        autoHeight: false,
+        autoplay: true,
+        responsive: {
+            320: {
+                items: 1,
+            },
+            768: {
+                items: 2,
+            },
+            992: {
+                items: 3,
+            }
+        }
+    });
+
+    /*------------------
+        Testimonial Slider
+    --------------------*/
+    $(".ts_slider").owlCarousel({
+        loop: true,
+        margin: 0,
+        items: 1,
+        dots: false,
+        nav: true,
+        navText: ['<i class="fa fa-angle-left"></i>', '<i class="fa fa-angle-right"></i>'],
+        smartSpeed: 1200,
+        autoHeight: false,
+        autoplay: true
+    });
+
+    /*------------------
+        Image Popup
+    --------------------*/
+    $('.image-popup').magnificPopup({
+        type: 'image'
+    });
+
+    /*------------------
+        Video Popup
+    --------------------*/
+    $('.video-popup').magnificPopup({
+        type: 'iframe'
+    });
+
+    /*------------------
+        Barfiller
+    --------------------*/
+    $('#bar1').barfiller({
+        barColor: '#ffffff',
+        duration: 2000
+    });
+    $('#bar2').barfiller({
+        barColor: '#ffffff',
+        duration: 2000
+    });
+    $('#bar3').barfiller({
+        barColor: '#ffffff',
+        duration: 2000
+    });
+
+
+
+
+    /*------------------
+        AOS Animation
+    --------------------*/
+    AOS.init({
+        duration: 800,
+        once: true,
+        offset: 100
+    });
+
+    /*------------------
+        Counter Animation
+    --------------------*/
+    var a = 0;
+    $(window).scroll(function() {
+        var counterSection = $('.counter-section');
+        if (counterSection.length > 0) {
+            var oTop = counterSection.offset().top - window.innerHeight;
+            if (a == 0 && $(window).scrollTop() > oTop) {
+                $('.counter').each(function() {
+                    var $this = $(this),
+                        countTo = $this.attr('data-target');
+                    $({
+                        countNum: $this.text()
+                    }).animate({
+                            countNum: countTo
+                        },
+                        {
+                            duration: 2000,
+                            easing: 'swing',
+                            step: function() {
+                                $this.text(Math.floor(this.countNum));
+                            },
+                            complete: function() {
+                                $this.text(this.countNum);
+                            }
+                        });
+                });
+                a = 1;
+            }
+        }
+    });
+
+})(jQuery);
